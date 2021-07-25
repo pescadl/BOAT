a4708e582504f8b7d46a9c0dede89ca9722b28ca
Justin Nguyen <justin@maidbot.com>
2021-07-25 18:32:09 -0500

    Woohoo timer triggered DAC LDMA sinewave works. And so does button triggered

---
+++ src/justins_test_main.c
@@ -0,0 +1,257 @@
+/***************************************************************************//**
+ * @file main_s1.c
+ * @brief This project uses the VDAC and TIMER0 to output a 32 point sine wave 
+ * at a particular frequency (10 kHz by default). This project operates in EM1.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Evaluation Quality
+ * This code has been minimally tested to ensure that it builds and is suitable 
+ * as a demonstration for evaluation purposes only. This code will be maintained
+ * at the sole discretion of Silicon Labs.
+ ******************************************************************************/
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_vdac.h"
+#include "em_timer.h"
+#include "em_ldma.h"
+#include "em_gpio.h"
+#include "em_prs.h"
+
+// 32 point sine table
+#define SINE_TABLE_SIZE 32
+static const uint16_t sineTable[SINE_TABLE_SIZE] = {
+  2048 , 2447 , 2831 , 3185 , 3495 , 3750 , 3939 , 4056 ,
+  4095 , 4056 , 3939 , 3750 , 3495 , 3185 , 2831 , 2447 ,
+  2048 , 1648 , 1264 , 910  , 600  , 345  , 156  , 39   ,
+  0    , 39   , 156  , 345  , 600  , 910  , 1264 , 1648 ,
+};
+
+// Note: change this to determine the frequency of the sine wave
+#define WAVEFORM_FREQ 10000
+
+// The timer needs to run at SINE_TABLE_SIZE times faster than the desired
+// waveform frequency because it needs to output SINE_TABLE_SIZE points in that
+// same amount of time
+#define TIMER0_FREQ (WAVEFORM_FREQ * SINE_TABLE_SIZE)
+
+/**************************************************************************//**
+ * @brief
+ *    VDAC initialization
+ *****************************************************************************/
+void initVdac(void)
+{
+  // Enable the VDAC clock
+  CMU_ClockEnable(cmuClock_VDAC0, true);
+
+  // Initialize the VDAC
+  VDAC_Init_TypeDef init = VDAC_INIT_DEFAULT;
+  init.prescaler = VDAC_PrescaleCalc(1000000, false, 0); // Use a 1 MHz VDAC
+  init.reference = vdacRef1V25Ln; // Set reference to internal 1.25V low noise
+  VDAC_Init(VDAC0, &init);
+
+  // Initialize VDAC channel 0
+  VDAC_InitChannel_TypeDef initChannel0 = VDAC_INITCHANNEL_DEFAULT;
+  VDAC_InitChannel(VDAC0, &initChannel0, 0);
+
+  // Set the settle time to zero for maximum update rate (mask it out)
+  VDAC0->OPA[0].TIMER &= ~(_VDAC_OPA_TIMER_SETTLETIME_MASK);
+
+  // Enable VDAC channel 0
+  VDAC_Enable(VDAC0, 0, true);
+}
+
+/**************************************************************************//**
+ * @brief
+ *    Timer initialization
+ *****************************************************************************/
+void initTimer(void)
+{
+  // Enable clock for TIMER0 module
+  CMU_ClockEnable(cmuClock_TIMER0, true);
+
+  // Initialize TIMER0
+  TIMER_Init_TypeDef init = TIMER_INIT_DEFAULT;
+  init.enable = false;
+  TIMER_Init(TIMER0, &init);
+
+  // Set top (reload) value for the timer
+  // Note: the timer runs off of the HFPER clock
+  uint32_t topValue = CMU_ClockFreqGet(cmuClock_HFPER) / TIMER0_FREQ;
+  TIMER_TopBufSet(TIMER0, topValue);
+
+  // Automatically clear the DMA request
+  TIMER0->CTRL |= TIMER_CTRL_DMACLRACT;
+
+  // Enable TIMER0
+  TIMER_Enable(TIMER0, true);
+}
+
+/**************************************************************************//**
+ * @brief
+ *    Initialize the LDMA module
+ *
+ * @details
+ *    Configure the channel descriptor to use the default memory to
+ *    peripheral transfer descriptor. Modify it to not generate an interrupt
+ *    upon transfer completion (we don't need it for this example).
+ *    Also make the descriptor link to itself so that the descriptor runs
+ *    continuously. Additionally, the transfer configuration selects the
+ *    TIMER0 overflow signal as the trigger for the DMA transaction to occur.
+ *
+ * @note
+ *    The descriptor object needs to at least have static scope persistence so
+ *    that the reference to the object is valid beyond its first use in
+ *    initialization. This is because this code loops back to the same
+ *    descriptor after every dma transfer. If the reference isn't valid anymore,
+ *    then all dma transfers after the first one will fail.
+ ******************************************************************************/
+void initLdma(void)
+{
+  // Descriptor loops through the sine table and outputs its values to the VDAC
+  static LDMA_Descriptor_t loopDescriptor =
+    LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&sineTable[0],   // Memory source address
+                                     &VDAC0->CH0DATA, // Peripheral destination address
+                                     SINE_TABLE_SIZE, // Number of bytes per transfer
+                                     0);              // Link to same descriptor
+  loopDescriptor.xfer.doneIfs = 0;             // Don't trigger interrupt when transfer is done
+  loopDescriptor.xfer.size = ldmaCtrlSizeHalf; // Transfer halfwords (VDAC data register is 12 bits)
+
+  // Transfer configuration and trigger selection
+  // Trigger on TIMER0 overflow
+  LDMA_TransferCfg_t transferConfig =
+    LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_TIMER0_UFOF);
+
+  // LDMA initialization
+  LDMA_Init_t init = LDMA_INIT_DEFAULT;
+  LDMA_Init(&init);
+
+  // Start the transfer
+  uint32_t channelNum = 0;
+  LDMA_StartTransfer(channelNum, &transferConfig, &loopDescriptor);
+}
+
+/**************************************************************************//**
+ * @brief Setup push button BTN0 as PRS source for DMAREQ0.
+ *****************************************************************************/
+static void gpioPrsSetup(void)
+{
+  /* Enable GPIO clock */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  /* Configure push button BTN0 as input */
+  const int button1 = 8;
+  GPIO_PinModeSet(gpioPortD, button1, gpioModeInputPullFilter, 1);
+
+  /* Configure interrupt on push button PB1 for rising edge but not enabled - PRS sensing instead */
+  GPIO_IntConfig(gpioPortD, button1, true, false, false);
+
+  /* Enable PRS clock */
+  CMU_ClockEnable(cmuClock_PRS, true);
+
+  /* Select GPIO as PRS source and push button BTN0 as signal for PRS channel 1 */
+  PRS_SourceSignalSet(1, PRS_CH_CTRL_SOURCESEL_GPIOH, button1, prsEdgePos);
+
+  /* Select PRS channel for DMA request 0 */
+  PRS->DMAREQ0 = PRS_DMAREQ0_PRSSEL_PRSCH1;
+}
+
+/**************************************************************************//**
+ * @brief
+ *    TODO
+ *****************************************************************************/
+void initLdmaPrs(void)
+{
+  // Descriptor loops through the sine table and outputs its values to the VDAC
+  static LDMA_Descriptor_t loopDescriptor =
+    LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&sineTable[0],   // Memory source address
+                                     &VDAC0->CH0DATA, // Peripheral destination address
+                                     SINE_TABLE_SIZE, // Number of halfwords per transfer
+                                     0);              // Link to same descriptor
+  loopDescriptor.xfer.doneIfs = 0;             // Don't trigger interrupt when transfer is done
+  loopDescriptor.xfer.size = ldmaCtrlSizeHalf; // Transfer halfwords (VDAC data register is 12 bits)
+
+  // Transfer configuration and trigger selection
+  // Trigger on TIMER0 overflow
+  LDMA_TransferCfg_t transferConfig =
+    LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_PRS_REQ0);
+
+  // LDMA initialization
+  LDMA_Init_t init = LDMA_INIT_DEFAULT;
+  LDMA_Init(&init);
+
+  // Start the transfer
+  uint32_t channelNum = 0;
+  LDMA_StartTransfer(channelNum, &transferConfig, &loopDescriptor);
+}
+
+/**************************************************************************//**
+ * @brief
+ *    TODO
+ *****************************************************************************/
+void disableMicrophone(void)
+{
+  /* Enable clock for GPIO module */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  // Set PA8 low to disable microphone
+  GPIO_PinModeSet(gpioPortA, 8, gpioModePushPull, 0);
+}
+
+/**************************************************************************//**
+ * @brief
+ *    Output a sine wave to DAC channel 0
+ *****************************************************************************/
+int main(void)
+{
+  // Chip errata
+  CHIP_Init();
+
+  // Init DCDC regulator with kit specific parameters
+  EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_DEFAULT;
+  EMU_DCDCInit(&dcdcInit);
+
+  // Need to disable microphone to make the mux select the DAC
+  disableMicrophone();
+
+  // Timer triggered DAC sinewave
+//  initVdac();
+//  initLdma();
+//  initTimer();
+
+  // Button triggered DAC sinewave
+//  initVdac();
+//  initLdmaPrs();
+//  gpioPrsSetup();
+
+  while (1) {
+    EMU_EnterEM1(); // Enter EM1 (won't exit)
+  }
+}
+
