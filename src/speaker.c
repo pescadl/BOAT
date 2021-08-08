561d6247c37524a3dbfc3ef1da6cb3eeb1b3fe1a
Justin Nguyen <justin@maidbot.com>
2021-08-08 14:33:15 -0500

    Yay honk at 96kHz

--- src/speaker.c
+++ src/speaker.c
@@ -9,38 +9,9 @@
 #include "../inc/speaker.h"
 #include "../inc/hardware.h"
 
-// 32 point sine table
-#define SINE_TABLE_SIZE 32
-static const uint16_t sineTable[SINE_TABLE_SIZE] = {
-  2048 , 2447 , 2831 , 3185 , 3495 , 3750 , 3939 , 4056 ,
-  4095 , 4056 , 3939 , 3750 , 3495 , 3185 , 2831 , 2447 ,
-  2048 , 1648 , 1264 , 910  , 600  , 345  , 156  , 39   ,
-  0    , 39   , 156  , 345  , 600  , 910  , 1264 , 1648 ,
-};
-
 /******************************************************************************
  *                              PRIVATE FUNCTIONS                             *
  ******************************************************************************/
-
-void VDAC0_IRQHandler(void)
-{
-  int flags = VDAC0->IF;
-  VDAC_IntClear(VDAC0, flags);
-  if (flags & VDAC_IF_CH0CD)
-  {
-    GPIO_PinOutToggle(gpioPortD, 0);
-  }
-  else if (flags & VDAC_IF_CH0BL)
-  {
-    GPIO_PinOutToggle(gpioPortD, 1);
-  }
-  else if ((flags & VDAC_IF_CH0UF) || (flags & VDAC_IF_CH0OF))
-//  else if (flags & VDAC_IF_CH0UF)
-  {
-//    GPIO_PinOutToggle(gpioPortD, 1);
-      GPIO_PinOutSet(gpioPortD, 1);
-  }
-}
 
 /******************************************************************************
  * @brief
@@ -68,18 +39,6 @@ static void initVdac(void)
   // Set the settle time to zero for maximum update rate (mask it out)
   VDAC0->OPA[0].TIMER &= ~(_VDAC_OPA_TIMER_SETTLETIME_MASK);
 
-  // TODO: remove
-  // Enable GPIO clock
-  CMU_ClockEnable(cmuClock_GPIO, true);
-  // Configure as output
-  GPIO_PinModeSet(gpioPortD, 0, gpioModePushPull, 0);
-  GPIO_PinModeSet(gpioPortD, 1, gpioModePushPull, 0);
-  NVIC_EnableIRQ(VDAC0_IRQn);
-  VDAC_IntEnable(VDAC0, VDAC_IEN_CH0CD);
-//  VDAC_IntEnable(VDAC0, VDAC_IEN_CH0BL);
-  VDAC_IntEnable(VDAC0, VDAC_IEN_CH0OF);
-  VDAC_IntEnable(VDAC0, VDAC_IEN_CH0UF);
-
   // Enable VDAC channel 0
   VDAC_Enable(VDAC0, 0, true);
 }
@@ -99,9 +58,9 @@ static void initLdma(void)
 {
   // Descriptor loops through the sine table and outputs its values to the VDAC
   static LDMA_Descriptor_t loopDescriptor =
-    LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&sineTable[0],
+    LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&HONK_WAVEFORM[0],
                                      &VDAC0->CH0DATA,
-                                     SINE_TABLE_SIZE,
+                                     HONK_WAVEFORM_SIZE2,
                                      0);
   // Don't trigger interrupt when transfer is done
   loopDescriptor.xfer.doneIfs = 0;
