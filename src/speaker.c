f4a9020644bb87daed4558f3adff76207e9854a3
Justin Nguyen <justin@maidbot.com>
2021-08-14 20:17:45 -0500

    Update LDMA descriptor logic to loop descriptor A (honk) for X times and then link to descriptor B to turn the VDAC off. Also robustified the MAX_XFER_COUNT macro using maths

--- src/speaker.c
+++ src/speaker.c
@@ -13,9 +13,15 @@
  *                                   MACROS                                   *
  ******************************************************************************/
 
-#define MAX_XFER_COUNT 2048
-#define NUM_LDMA_DESCRIPTORS (((int)(HONK_WAVEFORM_SIZE / MAX_XFER_COUNT))+1)
+// We want the maximum XFERCNT in decimal (e.g. 2^11 = 2048). We can achieve
+// this using some fancy maths
+#define MAX_XFER_COUNT ((_LDMA_CH_CTRL_XFERCNT_MASK >> _LDMA_CH_CTRL_XFERCNT_SHIFT) + 1)
 
+#define NUM_LDMA_DESCRIPTORS (NUM_LDMA_DESCRIPTORS_A + 2)
+#define NUM_LDMA_DESCRIPTORS_A (((int)(HONK_WAVEFORM_SIZE / MAX_XFER_COUNT))+1)
+#define NUM_LDMA_DESCRIPTORS_A_LOOPS 20
+#define LDMA_DESCRIPTOR_B_INDEX (NUM_LDMA_DESCRIPTORS - 2)
+#define LDMA_DESCRIPTOR_C_INDEX (NUM_LDMA_DESCRIPTORS - 1)
 
 
 /******************************************************************************
@@ -65,12 +71,21 @@ static void initVdac(void)
  ******************************************************************************/
 static void initLdma(void)
 {
-  // Create the array of linked descriptors. The max transfer size is not
-  // enough to play the entire sine table so we'll link the descriptors
+  // Descriptor sequence is:
+  // Loop(A0, A1, ..., An, NUM_LDMA_DESCRIPTORS_A_LOOPS), B, C
+  //  - A0 through An are for playing the honk sine wave
+  //  - We loop that for the desired loop count NUM_LDMA_DESCRIPTORS_A_LOOPS
+  //  - Then descriptor B will deactivate the VDAC
+  //  - Then descriptor C will deactivate the LDMA
+
+  // Create the array of linked descriptors
   static LDMA_Descriptor_t ldmaDescriptors[NUM_LDMA_DESCRIPTORS];
-  for (int i = 0; i < NUM_LDMA_DESCRIPTORS; i++)
+
+  // The max transfer size is not enough to play the entire sine table so
+  // we'll link the A-type descriptors together
+  for (int i = 0; i < NUM_LDMA_DESCRIPTORS_A; i++)
   {
-    if (i != NUM_LDMA_DESCRIPTORS-1)
+    if (i < NUM_LDMA_DESCRIPTORS_A-1)
     {
       // Each descriptor links to the next one
       ldmaDescriptors[i] = (LDMA_Descriptor_t)
@@ -84,11 +99,13 @@ static void initLdma(void)
       // Except the last descriptor, which links back to the first descriptor
       const uint16_t remainingTransfers =
         HONK_WAVEFORM_SIZE - (i*MAX_XFER_COUNT);
-      ldmaDescriptors[NUM_LDMA_DESCRIPTORS-1] = (LDMA_Descriptor_t)
+      ldmaDescriptors[i] = (LDMA_Descriptor_t)
         LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&HONK_WAVEFORM[i*MAX_XFER_COUNT],
                                          &VDAC0->CH0DATA,
                                          remainingTransfers,
-                                         -(NUM_LDMA_DESCRIPTORS-1));
+                                         -(NUM_LDMA_DESCRIPTORS_A-1));
+      // The last A-type descriptor will decrease the loop count
+      ldmaDescriptors[i].xfer.decLoopCnt = 1;
     }
     // Don't trigger interrupt when transfer is done
     ldmaDescriptors[i].xfer.doneIfs = 0;
@@ -96,9 +113,24 @@ static void initLdma(void)
     ldmaDescriptors[i].xfer.size = ldmaCtrlSizeHalf;
   }
 
+  // Set up descriptor B for deactivating the VDAC and link to the next
+  // descriptor
+  ldmaDescriptors[LDMA_DESCRIPTOR_B_INDEX] = (LDMA_Descriptor_t)
+    LDMA_DESCRIPTOR_SINGLE_WRITE(VDAC_CMD_CH0DIS,
+                                 &VDAC0->CMD);
+  ldmaDescriptors[LDMA_DESCRIPTOR_B_INDEX].wri.linkMode = ldmaLinkModeRel;
+  ldmaDescriptors[LDMA_DESCRIPTOR_B_INDEX].wri.link = 1;
+  ldmaDescriptors[LDMA_DESCRIPTOR_B_INDEX].wri.linkAddr = 1*4;
+
+  // Set up descriptor C for deactivating the LDMA
+  // TODO: ^
+//  ldmaDescriptors[LDMA_DESCRIPTOR_C_INDEX] = (LDMA_Descriptor_t)
+//      LDMA_DESCRIPTOR_SINGLE_WRITE(VDAC_CMD_CH0DIS,
+//                                   &VDAC0->CMD);
+
   // Trigger when VDAC0_CH0DATA is empty
-  LDMA_TransferCfg_t transferConfig =
-    LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_VDAC0_CH0);
+  LDMA_TransferCfg_t transferConfig = LDMA_TRANSFER_CFG_PERIPHERAL_LOOP(
+    ldmaPeripheralSignal_VDAC0_CH0, NUM_LDMA_DESCRIPTORS_A_LOOPS);
 
   // LDMA initialization
   LDMA_Init_t init = LDMA_INIT_DEFAULT;
@@ -107,7 +139,6 @@ static void initLdma(void)
   // Start the transfer
   uint32_t channelNum = 0;
   LDMA_StartTransfer(channelNum, &transferConfig, &ldmaDescriptors[0]);
-
 }
 
 /******************************************************************************
